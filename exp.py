ea901b16435ae272a8f29eb25d882a1614217605
Santosh Kumar Vishwakarma <santosh2alp@gmail.com>
2019-05-18 10:44:23 +0530

    added new table code

--- exp.py
+++ exp.py
@@ -1,3 +1,7 @@
 #! /data/data/com.termux/files/usr/bin/python3
-print ("Santosh")
-
+print ("HELLOW WORLD!")
+list1 = ["Santosh" , "Prabha" , "Rahul" , "Sunita" , "Adhya" , "Arya" , "Bhavya" ]
+print (len(list1))
+tab = int(input("Please input the number for table :"))
+for a in range (1,11):
+	print(a,"X",tab,"=",(tab*a))
