6e4bde01a96591193666c2e261cbec937112211c
Santosh Kumar Vishwakarma <santosh2alp@gmail.com>
2019-05-23 13:27:54 +0530

    Speech Recogntion app.

---
+++ 1exp.py
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/python3
+import speech_recognition as sr
+import pyaudio
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source :
+    print("lesinng .......")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print('you said : {} ' .format(text))
+    except:
+        print("could not recognize your voice")
+
